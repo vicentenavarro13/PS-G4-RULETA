960769b4464989555c678c75aab68def71d26a15
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2023-11-16 19:09:12 +0100

    Hecho un poco más bonito los layouts para que sea algo más vistosa la aplicación y añadida función de salida a botón en ModificacionActivity

--- app/src/main/java/com/example/caprichosa/ModficacionActivity.java
+++ app/src/main/java/com/example/caprichosa/ModficacionActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ModficacionActivity extends AppCompatActivity {
 
@@ -12,6 +16,15 @@ public class ModficacionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modficacion);
+        Button exitButton = findViewById(R.id.exitbutton);
+        Context self = this;
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(self, MainActivity.class);
+                startActivity(intent);
+            }
+        });
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         String[] result = {"Test111111111111111111111111111111111111111111111111111111","TEST","TEST","TEST","TEST","TEST","TEST","TEST","TEST","TEST","TEST"};
